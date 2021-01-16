dadb60e3fb1c7370b44e1fea2418592357edb0f1
Kara Krzystan <kara.krzystan@gmail.com>
2021-01-16 01:41:46 -0700

    Fixed issue with creating and displaying new notes

--- server.js
+++ server.js
@@ -5,35 +5,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const allNotes = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
-
-const allNotes = require('./db/db');
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ allNotes: notesArray }, null, 2)
-    );
-    return newNote;
-}
 
 app.get('/api/notes', (req, res) => {
     res.json(allNotes);
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = allNotes.length.toString();
-    //unique id needed, find package on npm 
-
-    const newNote = createNewNote(req.body, allNotes);
-    
-
-    res.json(newNote);
 });
 
 app.get('/', (req, res) => {
@@ -42,6 +21,29 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = allNotes.length.toString();
+    //unique id needed, find package on npm? 
+
+    const newNote = createNewNote(req.body, allNotes);
+    
+    res.json(newNote);
 });
 
 app.listen(PORT, () => {
