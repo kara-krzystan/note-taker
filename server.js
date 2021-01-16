285dda06b0d3ea61cef003215e459adf67434c43
Kara Krzystan <kara.krzystan@gmail.com>
2021-01-15 21:54:51 -0700

    Add middleware and test POST route

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
-const notes = require('./db/db');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+const allNotes = require('./db/db');
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.json(allNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
