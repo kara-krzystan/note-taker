d03d7fff18aa9323e06ca1c03478ee5ce3950e86
Kara Krzystan <kara.krzystan@gmail.com>
2021-01-17 22:03:36 -0700

    Add function to delete notes

--- server.js
+++ server.js
@@ -51,6 +51,27 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
