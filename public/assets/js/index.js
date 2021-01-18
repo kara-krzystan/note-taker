b11efec6713fde65b5347b4ecb6ab427e7f4eb62
Kara Krzystan <kara.krzystan@gmail.com>
2021-01-17 22:53:44 -0700

    Fixed 'activeNote.text' bug in index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -57,7 +57,7 @@ const renderActiveNote = () => {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
-    noteText.value = activeNote.title;
+    noteText.value = activeNote.text;
   } else {
     noteTitle.removeAttribute('readonly');
     noteText.removeAttribute('readonly');
